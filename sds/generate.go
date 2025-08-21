45106c938cdea759947524db7393acfca1b82f56
Ivan Folgueira Bande <ivansete@status.im>
2025-08-21 15:34:15 +0200

    better generate.go file

--- sds/generate.go
+++ sds/generate.go
@@ -1,8 +1,5 @@
-//go:build ignore
-// +build ignore
-
 package sds
 
 // This file contains the go:generate directive for building SDS native code.
 
-//go:generate sh -c "cd sds && make build"
+//go:generate sh -c "make build"
